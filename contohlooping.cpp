a6fd341aba54a164aaa611a7f9d0adbf617ab042
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-03-17 20:48:23 +0700

    contoh for descending

--- contohlooping.cpp
+++ contohlooping.cpp
@@ -18,6 +18,13 @@ int main() {
 
     cout << "Nilai counter terakhir = " << counter << endl << endl;
 
+    // Loop for descending
+    for (counter = 5; counter >= 0; counter--) {  
+        cout << "Nilai counter = " << counter << " TI UMY" << endl;
+    }
 
+    cout << "Nilai counter terakhir = " << counter << endl << endl;
+
+    
 
 }
