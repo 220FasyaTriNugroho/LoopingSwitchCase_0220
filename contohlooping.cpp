3b3ee4541a8827114ea29306c262d8a6681fda18
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-03-17 20:46:59 +0700

    contoh for ascending

--- contohlooping.cpp
+++ contohlooping.cpp
@@ -18,36 +18,6 @@ int main() {
 
     cout << "Nilai counter terakhir = " << counter << endl << endl;
 
-    // Loop for descending
-    for (counter = 5; counter >= 0; counter--) {  
-        cout << "Nilai counter = " << counter << " TI UMY" << endl;
-    }
 
-    cout << "Nilai counter terakhir = " << counter << endl << endl;
-
-    // Looping dengan while
-    srand(time(0));
-    i = rand() % 10;
-
-    cout << "Nilai awal i = " << i << endl;
-    while (i < 7) {
-        cout << "Nilai i = " << i << " TI UMY" << endl;
-        i = rand() % 10;
-        perulanganWhile++;
-    }
-
-    cout << "Jumlah perulangan = " << perulanganWhile << endl;
-    cout << "Nilai akhir i = " << i << endl << endl;
-
-    // Loop dengan do-while
-    cout << "Perulangan Do While = " << endl;
-    do {
-        cout << "Nilai i = " << i << " TI UMY" << endl;
-        i = rand() % 10;
-        perulanganDO++;
-    } while (i < 7); 
-
-    cout << "Jumlah Perulangan = " << perulanganDO << endl;
-    cout << "Nilai akhir i = " << i << endl;
 
 }
