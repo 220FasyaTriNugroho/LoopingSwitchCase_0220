a7e7a0f53a6223051c1c056fd9ab8baa0f8fd2b0
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-03-17 20:42:53 +0700

    Deklarasi Variabel

--- contohlooping.cpp
+++ contohlooping.cpp
@@ -1,58 +1,53 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-int main(){
-    int counter;
+int main() {
+    int counter; 
     int i;
     int perulanganWhile = 0;
-    int perulanganDo = 0;
-}
+    int perulanganDO = 0;
 
-//loop with for
-//loop for ascending
-for(counter = 0; counter < 5; counter++){
-    cout << "Nilai counter = " << counter << " TI UMY" <<  endl;
-}
-    cout << "Nilai counter terakhir = " << counter << endl;
-    cout << endl;
+    // Loop dengan for
+    // Loop for ascending
+    for (counter = 0; counter < 5; counter++) {  
+        cout << "Nilai counter = " << counter << " TI UMY" << endl;
+    }
 
-//loop for descending
-for(counter = 5; counter>) 0; counter--){
-    cout << "NIlai counter = " << counter << " TI UMY" << endl;
-}
-cout << "Nilai counter terakhir = " << counter 
+    cout << "Nilai counter terakhir = " << counter << endl << endl;
 
+    // Loop for descending
+    for (counter = 5; counter >= 0; counter--) {  
+        cout << "Nilai counter = " << counter << " TI UMY" << endl;
+    }
 
+    cout << "Nilai counter terakhir = " << counter << endl << endl;
 
-
-
-//loop with while
+    // Looping dengan while
     srand(time(0));
     i = rand() % 10;
 
     cout << "Nilai awal i = " << i << endl;
-    while (i < 7)
-    {
-        cout << "Nilai awal i = " << i << " TI UMY" << endl;
+    while (i < 7) {
+        cout << "Nilai i = " << i << " TI UMY" << endl;
         i = rand() % 10;
         perulanganWhile++;
     }
+
     cout << "Jumlah perulangan = " << perulanganWhile << endl;
-    cout << "Nilai akhir i = " << i << endl;
+    cout << "Nilai akhir i = " << i << endl << endl;
 
-    cout << endl;
-
-
-    //loop do while
-    cout << "Perulangan Do While"<< endl;
-    do
-    {
+    // Loop dengan do-while
+    cout << "Perulangan Do While = " << endl;
+    do {
         cout << "Nilai i = " << i << " TI UMY" << endl;
-        i = rand() %10;
-        perulanganDo++;
-    } while (i < 7);
-    cout << "Jumlah Perulangan = " << perulanganDo << endl;
+        i = rand() % 10;
+        perulanganDO++;
+    } while (i < 7); 
+
+    cout << "Jumlah Perulangan = " << perulanganDO << endl;
     cout << "Nilai akhir i = " << i << endl;
+
 }
